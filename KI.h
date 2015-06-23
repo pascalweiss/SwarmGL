e560722531db477136163c55f7494af4ec54434e
pascalweiss <pa-weiss@gmx.de>
2015-06-23 18:31:41 +0200

    added OpenGL initialization to KI.cpp

--- KI.h
+++ KI.h
@@ -1,6 +1,16 @@
 #pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 class KI
 {
+private:
+	GLuint programID;
+	GLFWwindow* window;
+	int initialize();
+	void error_callback(int error, const char* description);
+	void start();
 public:
 	KI(void);
 	~KI(void);
