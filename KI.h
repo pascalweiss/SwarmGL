2480a7748ea7844e13d5924f15abf5920d35f3f3
Tomatenbiss <s0545349@htw-berlin.de>
2015-06-30 17:43:02 +0200

    commit refresh files

--- KI.h
+++ KI.h
@@ -2,15 +2,20 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 class KI
 {
 private:
 	GLuint programID;
 	GLFWwindow* window;
+	glm::mat4 Projection;
+	glm::mat4 View;
+	glm::mat4 Model;
 	void init();
 	void initGLFW();
 	void initGLEW();
+	void sendMVP();
 	static void KI::error_callback(int error, const char* description) {
 		fputs(description, stderr);
 	}
--- SwarmGL.cpp
+++ SwarmGL.cpp
@@ -2,9 +2,12 @@
 
 
 #include "KI.h"
-
+#include "Particle.h"
 
 int main(void) {
 	KI *ki = new KI();
+	
 
+	delete ki;
+	
 }
