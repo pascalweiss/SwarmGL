5f5efd2bffb4fe883b161af2c58de6b843c6055b
Tomatenbiss <s0545349@htw-berlin.de>
2015-07-14 15:50:11 +0200

    Modified Globals.h

--- Globals.h
+++ Globals.h
@@ -1,7 +1,7 @@
 const int NUMBER_OF_PARTICLES = 200;
-const int DIMENSION_LENGTH = 15;
+const int DIMENSION_LENGTH = 11;
 const double PARTICLE_INFLUENCE = 1;
 const float PARTICLE_LENGTH = 0.3f;
 const bool DEBUG = false;
 const int THRESHOLD = 11;
-const float VELOCITY = 0.1;
+const float VELOCITY = 0.05;
